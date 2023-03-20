17ab872bbbdafe6151b6b52bb5a7e62521c3204a
blessingonye <bellex641@gmail.com>
2023-03-20 10:00:01 -0700

    print base

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,12 +3,14 @@
 /**
  * main - alx
  *
- * Return: 0 always
+ * Return: always 0
  */
 int main(void)
+
 {
 	int i;
-	
+	char j;
+
 	for (i = 0 ; i < 10 ; i++)
 		putchar(i + '0');
 	for (j = 'a' ; j <= 'f' ; j++)
