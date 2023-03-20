bb28e2f93a2612fb56eb2e4271cd1cb7c5944496
blessingonye <bellex641@gmail.com>
2023-03-20 03:20:45 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,7 +17,7 @@ printf("Size of an int: %lu byte(s)\n", (unsigned long)
 sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)
 sizeof(c));
-printf("Size of a iong long int: %lu byte(s)\n", (unsigned long)
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)
 sizeof(d));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)
 sizeof(e));
