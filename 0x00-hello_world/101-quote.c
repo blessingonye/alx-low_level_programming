293298d7a1c9a193a42c123963f6cb970b67f93a
blessingonye <bellex641@gmail.com>
2023-03-20 03:39:44 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+#include <unistd.h>
+/**
+ * main - A c program that prints a line to a standard error
+ * Return: 1 
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
